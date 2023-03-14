4c38c467346edf77863674576cdda18253ad217d
Simon Petrac <pfoerdie@gmail.com>
2023-03-14 13:26:48 +0100

    removed previous typescript experiments

--- types/util.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @template {typeof Error} ErrType
- * @param {Function} source
- * @param {typeof ErrType} ErrType
- * @param {...any} args
- * @throws {ErrType}
- */
-export function throwError<ErrType extends ErrorConstructor>(source: Function, ErrType: any, ...args: any[]): never;
-export function sealModule(target: any): void;
