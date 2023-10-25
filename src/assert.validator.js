79eb4233df23909d74b6bfb122f77b00e147cf26
Simon Petrac <pfoerdie@gmail.com>
2023-10-25 14:34:51 +0200

    added validators

---
+++ src/assert.validator.js
@@ -0,0 +1,93 @@
+const util = require('./util.js');
+
+/**
+ * @param {function (value: unknown): boolean} checker
+ * @returns {function (value: unknown): boolean}
+ */
+const validator = function (checker) {
+    if (!util.isFunction(checker) || !util.isBoolean(checker())) throw new Error('invalid checker');
+    return (value) => checker(value);
+};
+
+/**
+ * @template {string} T
+ * @param {RegExp} pattern
+ * @returns {function (value: unknown): value is T}
+ */
+validator.string = function (pattern) {
+    if (!util.isRegExp(pattern)) throw new Error('invalid pattern');
+    return (value) => util.isString(value) && pattern.test(value);
+};
+
+/**
+ * @template {Array<V>} T
+ * @template {any} V
+ * @param {function (value: unknown): value is V} checker
+ * @returns {function (value: unknown): value is T}
+ */
+validator.array = function (checker) {
+    if (!util.isFunction(checker)) throw new Error('invalid checker');
+    return (value) => util.isArray(value) && value.every(checker);
+};
+
+/**
+ * @template {any} T
+ * @param {Array<T>} choices
+ * @returns {function (value: unknown): value is T}
+ */
+validator.enum = function (choices) {
+    if (!util.isArray(choices)) throw new Error('invalid choices');
+    return (value) => choices.includes(value);
+};
+
+/**
+ * @template {Object} T
+ * @param {typeof T} classFunction
+ * @returns {function(value: unknown): value is T}
+ */
+validator.instance = function (classFunction) {
+    if (!util.isFunction(classFunction)) throw new Error('invalid classFunction');
+    return (value) => value instanceof classFunction;
+};
+
+/**
+ * @template {any} T
+ * @param {typeof T} datatype
+ * @returns {function(value: unknown): value is T}
+ */
+validator.datatype = function (datatype) {
+    if (!util.isDatatype(datatype)) throw new Error('invalid datatype');
+    return (value) => typeof value === datatype;
+};
+
+/**
+ * @template {any} T
+ * @param {Array<function (value: unknown): value is T>} concatenations
+ * @returns {function(value: unknown): value is T}
+ */
+validator.concatenation = function (concatenations) {
+    if (!util.isFunctionArray(concatenations)) throw new Error('invalid concatenations');
+    return (value) => concatenations.every(validator => validator(value));
+};
+
+/**
+ * @template {any} T
+ * @param {Array<function (value: unknown): value is T>} alternatives
+ * @returns {function(value: unknown): value is T}
+ */
+validator.alternative = function (alternatives) {
+    if (!util.isFunctionArray(alternatives)) throw new Error('invalid alternatives');
+    return (value) => alternatives.some(validator => validator(value));
+};
+
+/**
+ * @template {any} T
+ * @param {function (value: unknown): value is T} checker
+ * @returns {function(value: unknown): value is (null | T)}
+ */
+validator.optional = function (checker) {
+    if (!util.isFunction(checker)) throw new Error('invalid checker');
+    return (value) => util.isNull(value) || checker(value);
+};
+
+module.exports = validator;
--- src/util.js
+++ src/util.js
@@ -1,5 +1,16 @@
 const util = exports;
 
+const _datatypes = ['undefined', 'boolean', 'number', 'bigint', 'string', 'symbol', 'object', 'function'];
+
+util.isNull          = (value) => (value ?? null) === null;
+util.isArray         = (value) => Array.isArray(value);
+util.isRegExp        = (value) => value instanceof RegExp;
+util.isFunction      = (value) => typeof value === 'function';
+util.isFunctionArray = (value) => util.isArray(value) && value.every(util.isFunction);
+util.isDatatype      = (value) => _datatypes.includes(value);
+util.isString        = (value) => typeof value === 'string';
+util.isBoolean       = (value) => typeof value === 'boolean';
+
 /**
  * @template {typeof Error} ErrType
  * @param {Function} source
